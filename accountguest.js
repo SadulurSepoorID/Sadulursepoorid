6264c73dcbabcecb59053acc90af2b20903b1a22
SadulurSepoorID <sadulursepoor@gmail.com>
2025-04-11 15:52:39 +0700

    Add files via upload

---
+++ accountguest.js
@@ -0,0 +1,4 @@
+const validAccounts = [
+    { username: "Tamu website Sadulur Sepoor", kta: "Guest2025", password: "SS2025" },
+    { username: "Tamu website Sadulur Sepoor", kta: "GuestSS", password: "2025SS" }
+];
