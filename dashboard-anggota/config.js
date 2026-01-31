7360cef103b742d2401e5d917e595038933f6b32
sadulursepoorindonesia <sadulursepoor@gmail.com>
2026-02-01 00:36:00 +0700

    Update config.js

--- dashboard-anggota/config.js
+++ dashboard-anggota/config.js
@@ -1,2 +1,2 @@
 // GANTI URL INI DENGAN URL DEPLOY TERBARU KAMU (Cukup ganti di sini saja nantinya)
-const API_URL = "https://script.google.com/macros/s/AKfycbz6gZnHGJlElk9g7u-uRxYLPnIP3zHJMQOlIEmfP15hgSZLX2_kwzyXz4ov6nu-39mSlQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzXWzHOTjHH8pLJkvSCDorzgA9SaGJr09dmRs4CzFyZ-ceKUUQsCMyJqL2n2q1OkLpv/exec";
