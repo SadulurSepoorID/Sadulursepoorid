37dc84349d9b8bee6fc41dcc9474ed319eb8475e
sadulursepoorindonesia <sadulursepoor@gmail.com>
2026-03-18 04:36:44 +0700

    Delete accountguest.js

--- accountguest.js
+++
@@ -1,4 +0,0 @@
-const validAccounts = [
-    { username: "Tamu website Sadulur Sepoor", kta: "Guest2025", password: "SS2025" },
-    { username: "Tamu website Sadulur Sepoor", kta: "GuestSS", password: "2025SS" }
-];
